d3616d704d65410039fee4b0a07004a8fe0c0b70
이범희 <bhlee94@naver.com>
2022-10-06 15:06:57 +0900

    vue study

--- components/examples/Video.vue
+++ components/examples/Video.vue
@@ -2,26 +2,24 @@
   <div :class="text">
     <h2>{{text}}</h2>
     <h2>YouTube</h2>
-    <button @click="isShow = !isShow">youtube on/off</button>
+    <!-- <button @click="isShow = !isShow">youtube on/off</button> -->
     <!-- youtube 자동재생 설정: autoplay + 크롬자동재생 설정 -->
-    <iframe v-if="isShow" src="https://www.youtube.com/embed/APxCc13Bzng?autoplay=1&mute=1" title="레이저 바실리스크 X 하이퍼스피드 분해 조립" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe  src="https://www.youtube.com/embed/APxCc13Bzng?autoplay=1&mute=1" title="레이저 바실리스크 X 하이퍼스피드 분해 조립" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen width="1000" height="500"></iframe>
     <div style="margin-bottom: 100px;"></div>
     <h2>VideoJS</h2>
     <!-- videojs vue 용 -->
-    <video ref="videoPlayer" playsinline muted class="video-js"></video>
+    <video id="1" ref="videoPlayer" playsinline muted class="video-js"></video>
     <div style="margin-bottom: 100px;"></div>
     <!-- videojs static -->
     <video
-      id="my-video"
+      id="2"
       class="video-js"
       controls
       data-setup="{}"
-      autoplay
-      muted
     >
       <source src="https://d37hrtj0a49g18.cloudfront.net/2021-06/02_3233_46007/MP4/02_3233_46007_480p.mp4" type="video/mp4" />
       <source src="https://d37hrtj0a49g18.cloudfront.net/2021-06/02_3233_46007/MP4/02_3233_46007_480p.mp4" type="video/mp4" />
-      <p class="vjs-no-js">
+      <p class="1">
         하이메이드
         <a href="http://www.e-himart.co.kr/app/display/himadeGoodsShop" target="_blank">하이메이드관 레고레고</a>
       </p>
@@ -61,12 +59,15 @@ export default {
 
     // 화면 제어 => TODO: 스크롤 이벤트가 스크롤 시, 발생하지 않아서 확인 필요(overflow, body에 이벤트 주는 등 시도해보았지만, 제대로 잡히지 않았다)
     const handleScroll = (e) => {
+      console.log("scroll########################################");
       // console.log(e);
       // video object 찾기
       const objVideos = document.querySelectorAll('video');
+      const youtubes = document.querySelector("iframe").contentWindow;
+      console.log(youtubes);
       // console.log(objVideos);
       if (objVideos) {
-        console.log('video for >');
+        //console.log('video for >');
         // for (let video of document.querySelectorAll('video')) {
         for (let i = 0; i < objVideos.length; i++) {
           const video = objVideos[i];
@@ -82,8 +83,8 @@ export default {
             // 플레이 리스트에 동영상이 있는지 체크
             if (obj.playerId === addPlayerId) {
               isPlayList = true;
-              obj.yTop = video.getBoundingClientRect().top;   // 영상 상단 위치
-              obj.yBot = video.getBoundingClientRect().bottom;// 영상 하단 위치
+              // obj.yTop = video.getBoundingClientRect().top;   // 영상 상단 위치
+              // obj.yBot = video.getBoundingClientRect().bottom;// 영상 하단 위치
             }
           });
           // 플레이 리스트에 없으면, 목록 추가
@@ -106,17 +107,30 @@ export default {
           // 플레이 영역은 화면 1/10 ~ 9/10 영역에 영상 중앙이 위치해 있을 경우, 플레이 => TODO: 정확한 스크롤 위치가 잡히지 않아서 수정 필요
           const psblTop = /* 스크롤 위치(보여지는 화면 상단 기준) */ window.scrollY + /* 브라우저에 보여지는 상단부터 하단까지의 높이 */ window.innerHeight / 10;
           const psblBot = window.scrollY + window.innerHeight * 9 / 10;
-          console.log(`${yBot + ' > 0 && psblTop:' + psblTop + ' < yBot:' + yBot + ' && yTop:' + yTop + ' < psblBot:' + psblBot}`);
+          
           const isPlayY = yBot > 0 && psblTop < yBot && yTop < psblBot;
+          //const isPlayY = window.scrollY < psblTop && window.scrollY > yTop;
           if (isPlayY && !el.isPlaying) {
             console.log(`${el.playerId} is played.`);
             el.isPlaying = true;
             videojs(el.playerId).play();
+            console.log("yTop:::" + yTop);
+            console.log("yBot:::" + yBot);
+            console.log("psblTop:::" + psblTop);
+            console.log("psblBot:::" + psblBot);
+            console.log("scrollY:::" + window.scrollY);
+            console.log(`${yBot + ' > 0 && psblTop:' + psblTop + ' < yBot:' + yBot + ' && yTop:' + yTop + ' < psblBot:' + psblBot}`);
           } else if (!isPlayY && el.isPlaying) {
             // 플레이 영역이 아닌 영상이 플레이 중일 때, 일시정지
             console.log(`${el.playerId} is paused.`);
             el.isPlaying = false;
             videojs(el.playerId).pause();
+            console.log("yTop:::" + yTop);
+            console.log("yBot:::" + yBot);
+            console.log("psblTop:::" + psblTop);
+            console.log("psblBot:::" + psblBot);
+            console.log("scrollY:::" + window.scrollY);
+            console.log(`${yBot + ' > 0 && psblTop:' + psblTop + ' < yBot:' + yBot + ' && yTop:' + yTop + ' < psblBot:' + psblBot}`);
           }
         });
       }
